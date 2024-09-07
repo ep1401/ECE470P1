cd5eb2e3b2671675ee77ddd45dbe535ba2271c06
Ethan Puckett <ethanpuckett@dynamic-oit-ip4-wifirestricted03-10-49-74-48.princeton.edu>
2024-09-06 22:20:46 -0400

    finished exercises

--- rustlings_exercises/options1.rs
+++ rustlings_exercises/options1.rs
@@ -8,7 +8,13 @@
         // value of 0 The Option output should gracefully handle cases where
         // time_of_day > 23.
         // TODO: Complete the function body - remember to return an Option!
-        ???
+        if time_of_day > 23 {
+            None
+        } else if time_of_day < 22 {
+            Some(5) 
+        } else {
+            Some(0) 
+        }
     }
     
     #[cfg(test)]
@@ -29,7 +35,7 @@
             // TODO: Fix this test. How do you get at the value contained in the
             // Option?
             let icecreams = maybe_icecream(12);
-            assert_eq!(icecreams, 5);
+            assert_eq!(icecreams, Some(5));
         }
     }
     
--- rustlings_exercises/tests3.rs
+++ rustlings_exercises/tests3.rs
@@ -16,12 +16,17 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(2));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(!is_even(1));
+    }
+
+    #[test]
+    fn is_five_even() {
+        assert_eq!(is_even(5), false);
     }
 }
 
